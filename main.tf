8b1174dac1e6438472e69af0c8dd5c5ce54eced3
lapCinte <juro_lc@hotmail.com>
2025-08-22 14:03:03 -0500

    viernes

--- main.tf
+++ main.tf
@@ -217,99 +217,99 @@ locals {
 
 
 #Create Application Gateway
-resource "azurerm_application_gateway" "application_gateway" {
-  name                = "MyApplicationGateway"
-  location            = azurerm_resource_group.MyResourceGroup.location
-  resource_group_name = azurerm_resource_group.MyResourceGroup.name
+# resource "azurerm_application_gateway" "application_gateway" {
+#   name                = "MyApplicationGateway"
+#   location            = azurerm_resource_group.MyResourceGroup.location
+#   resource_group_name = azurerm_resource_group.MyResourceGroup.name
 
-  sku {
-    name     = "Standard_v2"
-    tier     = "Standard_v2"
-    capacity = 2
-  }
+#   sku {
+#     name     = "Standard_v2"
+#     tier     = "Standard_v2"
+#     capacity = 2
+#   }
 
-  gateway_ip_configuration {
-    name      = "gatewayIpConfig"
-    subnet_id = azurerm_subnet.subnet_ag.id
-  }
+#   gateway_ip_configuration {
+#     name      = "gatewayIpConfig"
+#     subnet_id = azurerm_subnet.subnet_ag.id
+#   }
 
-  frontend_port {
-    name = local.frontend_port_name
-    port = 80
-  }
+#   frontend_port {
+#     name = local.frontend_port_name
+#     port = 80
+#   }
 
-  frontend_ip_configuration {
-    name                 = local.frontend_ip_configuration_name
-    public_ip_address_id = azurerm_public_ip.public_ip_ag.id
-  }
+#   frontend_ip_configuration {
+#     name                 = local.frontend_ip_configuration_name
+#     public_ip_address_id = azurerm_public_ip.public_ip_ag.id
+#   }
 
-  backend_address_pool {
-    name = local.backend_address_pool_name_docker
-  }
-  backend_address_pool {
-    name = local.backend_address_pool_name_otras
-  }
+#   backend_address_pool {
+#     name = local.backend_address_pool_name_docker
+#   }
+#   backend_address_pool {
+#     name = local.backend_address_pool_name_otras
+#   }
 
-  backend_http_settings {
-    name                  = local.http_settings_name_docker
-    cookie_based_affinity = "Disabled"
-    port                  = 8081
-    protocol              = "Http"
-    request_timeout       = 20
-  }
-  backend_http_settings {
-    name                  = local.http_settings_name_otras
-    cookie_based_affinity = "Disabled"
-    port                  = 8080
-    protocol              = "Http"
-    request_timeout       = 20
-    pick_host_name_from_backend_address = true
-    probe_name = local.health_probe_name_otras
+#   backend_http_settings {
+#     name                  = local.http_settings_name_docker
+#     cookie_based_affinity = "Disabled"
+#     port                  = 8081
+#     protocol              = "Http"
+#     request_timeout       = 20
+#   }
+#   backend_http_settings {
+#     name                  = local.http_settings_name_otras
+#     cookie_based_affinity = "Disabled"
+#     port                  = 8080
+#     protocol              = "Http"
+#     request_timeout       = 20
+#     pick_host_name_from_backend_address = true
+#     probe_name = local.health_probe_name_otras
 
-  }
+#   }
 
-  http_listener {
-    name                           = local.listener_name
-    frontend_ip_configuration_name = local.frontend_ip_configuration_name
-    frontend_port_name             = local.frontend_port_name
-    protocol                       = "Http"
-  }
+#   http_listener {
+#     name                           = local.listener_name
+#     frontend_ip_configuration_name = local.frontend_ip_configuration_name
+#     frontend_port_name             = local.frontend_port_name
+#     protocol                       = "Http"
+#   }
 
-  url_path_map {
-    name                = local.url_path_map_name
-    default_backend_address_pool_name  = local.backend_address_pool_name_docker
-    default_backend_http_settings_name = local.http_settings_name_docker
+#   url_path_map {
+#     name                = local.url_path_map_name
+#     default_backend_address_pool_name  = local.backend_address_pool_name_docker
+#     default_backend_http_settings_name = local.http_settings_name_docker
 
-    path_rule {
-      name                       = "otrasAppsPathRule"
-      paths                      = ["/otras*"]
-      backend_address_pool_name  = local.backend_address_pool_name_otras
-      backend_http_settings_name = local.http_settings_name_otras
-    }
-  }
+#     path_rule {
+#       name                       = "otrasAppsPathRule"
+#       paths                      = ["/otras*"]
+#       backend_address_pool_name  = local.backend_address_pool_name_otras
+#       backend_http_settings_name = local.http_settings_name_otras
+#     }
+#   }
 
-  request_routing_rule {
-    name                       = local.request_routing_rule_name
-    priority                   = 100
-    rule_type                  = "PathBasedRouting"
-    url_path_map_name          = local.url_path_map_name
-    http_listener_name         = local.listener_name
-  }
-  probe {
-    name                = local.health_probe_name_otras
-    protocol            = "Http"
-    path                = "/otras/login"
-    interval            = 30
-    timeout             = 30
-    unhealthy_threshold = 3
-    pick_host_name_from_backend_http_settings =  true
-    match {
-      status_code = ["200-403"]
-    }
+#   request_routing_rule {
+#     name                       = local.request_routing_rule_name
+#     priority                   = 100
+#     rule_type                  = "PathBasedRouting"
+#     url_path_map_name          = local.url_path_map_name
+#     http_listener_name         = local.listener_name
+#   }
+#   probe {
+#     name                = local.health_probe_name_otras
+#     protocol            = "Http"
+#     path                = "/otras/login"
+#     interval            = 30
+#     timeout             = 30
+#     unhealthy_threshold = 3
+#     pick_host_name_from_backend_http_settings =  true
+#     match {
+#       status_code = ["200-403"]
+#     }
 
-  }
+#   }
 
-  tags = {
-    environment = "pruebas"
-  }
-}
+#   tags = {
+#     environment = "pruebas"
+#   }
+# }
